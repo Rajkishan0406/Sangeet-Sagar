4d9268e69ad16f8d22a1880302af18edd55a4e5a
Rajkishan0406 <rajkishan8298328476@gmail.com>
2021-03-02 12:38:48 -0500

    Delete and Item full description done

--- app/src/main/java/com/example/sangeetsagarowner/MainDashboard/ItemFullDescription.kt
+++ app/src/main/java/com/example/sangeetsagarowner/MainDashboard/ItemFullDescription.kt
@@ -1,17 +1,23 @@
 package com.example.sangeetsagarowner.MainDashboard
 
 import android.content.Context
+import android.content.Intent
 import android.content.SharedPreferences
+import android.graphics.Color
 import android.os.Bundle
 import android.preference.PreferenceManager
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ProgressBar
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.cardview.widget.CardView
 import androidx.fragment.app.Fragment
 import com.example.sangeetsagarowner.R
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.android.gms.tasks.OnFailureListener
 import com.google.firebase.database.*
 import org.w3c.dom.Text
 
@@ -25,6 +31,10 @@ class ItemFullDescription :Fragment(){
     lateinit var brand : TextView
     lateinit var power : TextView
     lateinit var about : TextView
+    lateinit var card : CardView
+    lateinit var avatext : TextView
+    lateinit var progress  :ProgressBar
+    lateinit var delete : CardView
 
     lateinit var sharedPreferences: SharedPreferences
     lateinit var database : DatabaseReference
@@ -54,11 +64,53 @@ class ItemFullDescription :Fragment(){
         brand = view.findViewById(R.id.brand)
         power = view.findViewById(R.id.power)
         about = view.findViewById(R.id.about)
+        card = view.findViewById(R.id.available_cardview)
+        avatext = view.findViewById(R.id.ava_text)
+        progress = view.findViewById(R.id.progress_full)
+        delete = view.findViewById(R.id.delete)
+
+        progress.visibility = View.VISIBLE
 
         database = item_father?.let { FirebaseDatabase.getInstance().getReference("Products").child(it) }!!
 
+        database.child(key.toString()).addValueEventListener(object : ValueEventListener{
+            override fun onDataChange(snapshot: DataSnapshot) {
+                if(snapshot.exists()){
+                    model.setText(snapshot.child("Model").getValue() as String)
+                    price.setText(snapshot.child("Price").getValue() as String)
+                    weight.setText(snapshot.child("Weight").getValue() as String)
+                    brand.setText(snapshot.child("Brand").getValue() as String)
+                    power.setText(snapshot.child("Power").getValue() as String)
+                    about.setText(snapshot.child("Describe").getValue() as String)
+                    var checker = snapshot.child("Availability").getValue() as String
+                    if(checker.equals("0")){
+                        card.setCardBackgroundColor(Color.RED)
+                        avatext.setText("Not Available")
+                    }
+                    progress.visibility = View.INVISIBLE
+                }
+            }
 
+            override fun onCancelled(error: DatabaseError) {
 
+            }
+
+        })
+
+        delete.setOnClickListener(View.OnClickListener {
+            progress.visibility = View.VISIBLE
+            var data : DatabaseReference
+            data = FirebaseDatabase.getInstance().getReference("Products").child(item_father).child(key.toString())
+            data.removeValue().addOnCompleteListener(OnCompleteListener {
+                progress.visibility = View.INVISIBLE
+                Toast.makeText(activity,"Product Deleted Successfully",Toast.LENGTH_SHORT).show()
+                val intent = Intent(getActivity(), Dashboard::class.java)
+                getActivity()?.startActivity(intent)
+            }).addOnFailureListener(OnFailureListener {
+                progress.visibility = View.INVISIBLE
+                Toast.makeText(activity,"Something went wrong",Toast.LENGTH_SHORT).show()
+            })
+        })
 
         return view
     }
