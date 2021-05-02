9bc875335eff82e9c8c10611a6219ebe4f27b44a
Rajkishan0406 <rajkishan8298328476@gmail.com>
2021-05-02 13:48:30 -0400

    Reset prod/brand code done!!

--- app/src/main/java/com/example/sangeetsagarowner/NavigationMenuDrawer/Product_Comparision.kt
+++ app/src/main/java/com/example/sangeetsagarowner/NavigationMenuDrawer/Product_Comparision.kt
@@ -12,6 +12,7 @@ import android.widget.Toast
 import androidx.cardview.widget.CardView
 import androidx.fragment.app.Fragment
 import com.example.sangeetsagarowner.R
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import com.google.firebase.database.*
 
 class Product_Comparision : Fragment() {
@@ -28,6 +29,7 @@ class Product_Comparision : Fragment() {
     var sum = 0 as Int
     var max = 0 as Int
     var ans = "" as String
+    var item_deleted = 0 as Int
 
     lateinit var item_sum : TextView
     lateinit var Brand_sum : TextView
@@ -51,8 +53,48 @@ class Product_Comparision : Fragment() {
         Brand_sum = view.findViewById(R.id.sold_brand_textview_answer)
         heighest = view.findViewById(R.id.heighest_sold_item)
 
-        reset_brand.setOnClickListener(View.OnClickListener {
+        reset_prod.setOnClickListener(View.OnClickListener {
+            var bb = brand.text.toString()
+            var md = model.text.toString()
+            if (bb.length != 0 && md.length != 0) {
+                MaterialAlertDialogBuilder(this.requireContext())
+                    .setTitle("Alert")
+                    .setMessage("By Reseting Product sold data will be deleted")
+                    .setNegativeButton("Cancel") { dialog, which ->
+                        Log.i("Message : ", "Canceled")
+                    }
+                    .setPositiveButton("Reset") { dialog, which ->
+                        Log.i("Message : ", "Deleted")
+                        item_deleted = 1
+                        brand_sum = 0;
+                        Brand_sum.setText("--")
+                        item_sum.setText("--")
+                        database!!.child(bb!!).child(md!!).removeValue()
+                    }
+                    .show()
+            }
+        })
 
+        reset_brand.setOnClickListener(View.OnClickListener {
+            var bb = brand.text.toString()
+            var md = model.text.toString()
+            if (bb.length != 0 && md.length != 0) {
+                MaterialAlertDialogBuilder(this.requireContext())
+                    .setTitle("Alert")
+                    .setMessage("By Reseting all Brand sold data will be deleted")
+                    .setNegativeButton("Cancel") { dialog, which ->
+                        Log.i("Message : ", "Canceled")
+                    }
+                    .setPositiveButton("Reset") { dialog, which ->
+                        Log.i("Message : ", "Deleted")
+                        item_deleted = 1
+                        brand_sum = 0;
+                        Brand_sum.setText("--")
+                        item_sum.setText("--")
+                        database!!.child(bb!!).removeValue()
+                    }
+                    .show()
+            }
         })
 
         search.setOnClickListener(View.OnClickListener {
@@ -62,6 +104,7 @@ class Product_Comparision : Fragment() {
             brand_sum = 0;
             Brand_sum.setText("--")
             item_sum.setText("--")
+            item_deleted = 0;
             if (bb.length == 0 && md.length == 0){
                 Toast.makeText(activity, "Please enter the fields", Toast.LENGTH_SHORT).show()
                 heighest.setText("No Brand Selected")
@@ -69,7 +112,7 @@ class Product_Comparision : Fragment() {
             else{
                 database.child(bb).orderByKey().addValueEventListener(object : ValueEventListener{
                     override fun onDataChange(snapshot: DataSnapshot) {
-                        if(snapshot.exists()){
+                        if(snapshot.exists() && item_deleted == 0){
                             for(h in snapshot.children){
                                 var MODEL = h.key as String
                                 take_model_sum(MODEL,bb)
@@ -81,6 +124,7 @@ class Product_Comparision : Fragment() {
 
                         else{
                             prog.visibility = View.INVISIBLE
+                            if(item_deleted ==0 )
                             Toast.makeText(activity,"Above Brand Name is not added in Sold Product",Toast.LENGTH_SHORT).show()
                             heighest.setText("No Brand Selected")
                         }
@@ -101,10 +145,10 @@ class Product_Comparision : Fragment() {
     private fun take_model_sum(model: String, bb: String) {
         database.child(bb).child(model).orderByValue().addValueEventListener(object : ValueEventListener{
             override fun onDataChange(snapshot: DataSnapshot) {
-                if(snapshot.exists()){
+                if(snapshot.exists() && item_deleted == 0){
                     var Num = snapshot.child("Quantity").getValue() as String
                     var num = Num.toInt()
-                    if(num > max) {
+                    if(num >= max) {
                         max = num
                         ans = model
                         var peak = ans + ":" + max.toString()
@@ -127,10 +171,10 @@ class Product_Comparision : Fragment() {
     private fun take_item_sum(md: String, bb: String) {
         database.child(bb).addValueEventListener(object : ValueEventListener{
             override fun onDataChange(snapshot: DataSnapshot) {
-                if(snapshot.exists()){
-                    var Sum = snapshot.child(md).child("Quantity").getValue() as String
+                if(snapshot.exists() && item_deleted == 0){
+                    var Sum = snapshot.child(md!!).child("Quantity").getValue() as String
                     sum = Sum.toInt()
-                    item_sum?.setText(snapshot.child(md).child("Quantity").getValue() as String)
+                    item_sum?.setText(snapshot.child(md!!).child("Quantity").getValue() as String)
                 }
             }
 
