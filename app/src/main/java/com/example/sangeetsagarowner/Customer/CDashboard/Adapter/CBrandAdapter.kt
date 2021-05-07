be6894d7668c2661fa59061d29ed6a0fe6a9aeb8
Rajkishan0406 <rajkishan8298328476@gmail.com>
2021-05-06 21:10:34 -0400

    Brand collection done for customer completely!!

---
+++ app/src/main/java/com/example/sangeetsagarowner/Customer/CDashboard/Adapter/CBrandAdapter.kt
@@ -0,0 +1,39 @@
+package com.example.sangeetsagarowner.Customer.CDashboard.Adapter
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.cardview.widget.CardView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.sangeetsagarowner.MainDashboard.Adapter.Brand_Adapter
+import com.example.sangeetsagarowner.MainDashboard.Model.Brand_Model
+import com.example.sangeetsagarowner.R
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+
+class CBrand_Adapter(var brandlist : ArrayList<Brand_Model>) : RecyclerView.Adapter<CBrand_Adapter.ViewHolder>() {
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val v = LayoutInflater.from(parent.context).inflate(R.layout.cbrand_list_layout,parent,false)
+        return ViewHolder(v)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val brand : Brand_Model = brandlist[position]
+        holder.texting.text = brand.brand
+
+    }
+
+    override fun getItemCount(): Int {
+        return brandlist.size
+    }
+
+    class ViewHolder(itemView : View) : RecyclerView.ViewHolder(itemView){
+        var texting = itemView.findViewById(R.id.item_name_of_product) as TextView
+        var card  = itemView.findViewById(R.id.item_card) as CardView
+
+    }
+}
--- app/src/main/java/com/example/sangeetsagarowner/Customer/CSetting/CBrand.kt
+++ app/src/main/java/com/example/sangeetsagarowner/Customer/CSetting/CBrand.kt
@@ -4,11 +4,25 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ProgressBar
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.sangeetsagarowner.Customer.CDashboard.Adapter.CBrand_Adapter
+import com.example.sangeetsagarowner.MainDashboard.Adapter.Brand_Adapter
+import com.example.sangeetsagarowner.MainDashboard.Add_Brand_Name
+import com.example.sangeetsagarowner.MainDashboard.Model.Brand_Model
 import com.example.sangeetsagarowner.R
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.firebase.database.*
 
 class CBrand : Fragment() {
+
+
+    lateinit var recyclerView: RecyclerView
+    lateinit var progress : ProgressBar
+    lateinit var database : DatabaseReference
 
     override fun onStart() {
         super.onStart()
@@ -20,6 +34,39 @@ class CBrand : Fragment() {
             container: ViewGroup?,
             savedInstanceState: Bundle?): View? {
         var view = inflater.inflate(R.layout.cbrand, container, false)
+
+
+        database = FirebaseDatabase.getInstance().getReference("Brand")
+        recyclerView = view.findViewById(R.id.brand_recyclerview)
+        recyclerView.setHasFixedSize(true)
+        recyclerView.layoutManager = LinearLayoutManager(activity)
+        progress = view.findViewById(R.id.brand_progressbar)
+
+        var itemname = ArrayList<Brand_Model>()
+
+        database?.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                if(snapshot!!.exists()){
+                    itemname.clear()
+                    for( h in snapshot.children){
+                        val name = h.getValue()
+                        itemname.add(Brand_Model(name as String))
+                    }
+                    progress.visibility = View.INVISIBLE
+                    val adapter = CBrand_Adapter(itemname)
+                    recyclerView.adapter = adapter
+                }
+                else{
+                    progress.visibility = View.INVISIBLE
+                }
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+
+            }
+
+        })
+
 
         return view
     }
